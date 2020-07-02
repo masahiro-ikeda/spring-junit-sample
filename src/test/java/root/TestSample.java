80faa0612314dd9546deb57ca6aefa456c8d751e
masahiro-ikeda <masahiro.ikeda@careritz.co.jp>
2020-07-03 02:28:31 +0900

    first commit

---
+++ src/test/java/root/TestSample.java
@@ -0,0 +1,15 @@
+package root;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class TestSample {
+
+  @Test
+  public void test() {
+    Assertions.assertTrue( true );
+  }
+}
